c155c967ee8b46f2335b727189c199b4b2f2bcb7
Jack Wimbish <wimbishjack@gmail.com>
2025-07-14 21:34:21 -0500

    add multiplayer

--- game_board.gd
+++ game_board.gd
@@ -1,5 +1,6 @@
 extends Control
 
+const DEFAULT_PORT = 9999
 const CardScene = preload("res://card.tscn")
 
 # Our simple card database
@@ -23,16 +24,13 @@ const CARD_DATA = {
 
 func _on_card_clicked(card_node):
 	print("A card was selected: ", card_node.get_node("VBoxContainer/CardName").text)
-
-func add_card_to_hand(card_id):
-	# Get the specific card's data from our database
-	var data = CARD_DATA[card_id]
-
-	var new_card = CardScene.instantiate()
 	
-	# Call the new setup function on the card
+@rpc("any_peer", "call_local")
+func add_card_to_hand(card_id):
+	# The rest of the function is the same as before
+	var data = CARD_DATA[card_id]
+	var new_card = CardScene.instantiate()
 	new_card.setup_card_data(data)
-
 	new_card.card_clicked.connect(_on_card_clicked)
 	$PlayerHand.add_child(new_card)
 
@@ -42,3 +40,26 @@ func _ready():
 	add_card_to_hand("novice_engineer")
 	add_card_to_hand("fireball")
 	add_card_to_hand("frostbolt")
+
+
+func _on_host_button_pressed() -> void:
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_server(DEFAULT_PORT)
+	multiplayer.multiplayer_peer = peer
+	print("Server started. Waiting for players.")
+
+
+func _on_join_button_pressed() -> void:
+	var ip = $NetworkUI/IPAddressField.text
+	if ip == "":
+		ip = "127.0.0.1" # Default to localhost for easy testing
+	
+	var peer = ENetMultiplayerPeer.new()
+	peer.create_client(ip, DEFAULT_PORT)
+	multiplayer.multiplayer_peer = peer
+	print("Joining server at ", ip)
+
+
+func _on_draw_button_pressed() -> void:
+	# Instead of calling the function directly, we call the RPC
+	add_card_to_hand.rpc("fireball")
