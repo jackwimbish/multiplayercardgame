e58329d4ab974e4bd8f8761671df5858387985e2
Jack Wimbish <wimbishjack@gmail.com>
2025-07-14 21:56:17 -0500

    card drag-drop

--- card.gd
+++ card.gd
@@ -1,8 +1,25 @@
 extends PanelContainer
 signal card_clicked
+
+signal drag_started(card)
+signal dropped(card)
+
+var is_dragging = false
+var drag_offset = Vector2.ZERO
+
 func _gui_input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		card_clicked.emit(self)
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			is_dragging = true
+			drag_offset = get_global_mouse_position() - global_position
+			drag_started.emit(self)
+		else: # Mouse button released
+			is_dragging = false
+			dropped.emit(self)
+			card_clicked.emit(self)
+
+	if event is InputEventMouseMotion and is_dragging:
+		global_position = get_global_mouse_position() - drag_offset
 
 func setup_card_data(data):
 	# "data" is a Dictionary with card info
